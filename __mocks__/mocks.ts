ee1b1d28dbc8dbf484f0ccda5d5a8345f2d660fc
Artem Lobovskii <lobovskiy@yandex.ru>
2024-02-13 00:36:42 +0300

    feat: add test scenario 2 and errors test scenario

--- __mocks__/mocks.ts
+++ __mocks__/mocks.ts
@@ -5,3 +5,15 @@ export const mockUserData1: UserData = {
   age: 22,
   hobbies: ['js', 'node'],
 };
+
+export const mockUserData2: UserData = {
+  username: 'Geek 2',
+  age: 24,
+  hobbies: ['react', 'angular'],
+};
+
+export const mockUserData3: UserData = {
+  username: 'Geek 3',
+  age: 25,
+  hobbies: ['aws', 'kubernetes'],
+};
--- __tests___/app.test.ts
+++ __tests___/app.test.ts
@@ -2,10 +2,15 @@ import fsPromises from 'fs/promises';
 import { join } from 'path';
 import { createServer } from 'http';
 import request from 'supertest';
+import { v4 } from 'uuid';
 import App from '../src/App';
 import { BASE_URL, JSON_DB_PATH } from '../src/consts';
 import { IUser } from '../src/types';
-import { mockUserData1 } from '../__mocks__/mocks';
+import {
+  mockUserData1,
+  mockUserData2,
+  mockUserData3,
+} from '../__mocks__/mocks';
 
 const jsonDbFilePath = join(__dirname, JSON_DB_PATH);
 const app = new App(jsonDbFilePath);
@@ -21,7 +26,7 @@ afterAll(async () => {
   await fsPromises.unlink(jsonDbFilePath);
 });
 
-describe('positive CRUD operations scenario', () => {
+describe('CRUD operations scenario 1', () => {
   let newUser: IUser;
 
   it('should get an empty array', () => {
@@ -69,5 +74,124 @@ describe('positive CRUD operations scenario', () => {
 
   it('should delete user', () => {
     return request(server).delete(`${BASE_URL}/${newUser.id}`).expect(204);
+  });
+});
+
+describe('CRUD operations scenario 2', () => {
+  let newUser1: IUser;
+  let newUser2: IUser;
+
+  it('should create new user', () => {
+    return request(server)
+      .post(BASE_URL)
+      .send(mockUserData2)
+      .expect(201)
+      .then((res) => {
+        expect(res.body).toEqual({ ...mockUserData2, id: res.body.id });
+
+        newUser1 = res.body;
+      });
+  });
+
+  it('should create another new user', () => {
+    return request(server)
+      .post(BASE_URL)
+      .send(mockUserData3)
+      .expect(201)
+      .then((res) => {
+        expect(res.body).toEqual({ ...mockUserData3, id: res.body.id });
+
+        newUser2 = res.body;
+      });
+  });
+
+  it('should get an array with two users', () => {
+    return request(server)
+      .get(BASE_URL)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.length).toEqual(2);
+      });
+  });
+
+  it('should return 400 for PUT user with invalid data', () => {
+    return request(server)
+      .put(`${BASE_URL}/${newUser1.id}`)
+      .send({ invalidData: false })
+      .expect(400)
+      .then((res) => {
+        expect(res.body.error).toEqual('Bad request');
+      });
+  });
+
+  it('should delete the first user', () => {
+    return request(server).delete(`${BASE_URL}/${newUser1.id}`).expect(204);
+  });
+
+  it('should get an array with the second user only', () => {
+    return request(server)
+      .get(BASE_URL)
+      .expect(200)
+      .then((res) => {
+        expect(res.body.length).toEqual(1);
+        expect(res.body[0]).toEqual(newUser2);
+      });
+  });
+});
+
+describe('errors CRUD operations scenario', () => {
+  it('should return 501 for unsupported method', () => {
+    return request(server).options(BASE_URL).expect(501);
+  });
+
+  it('should return 404 for GET invalid resource path', () => {
+    return request(server).get('/qwerty').expect(404);
+  });
+
+  it('should return 404 for GET non-existent user id', () => {
+    return request(server)
+      .get(`${BASE_URL}/${v4()}`)
+      .expect(404)
+      .then((res) => {
+        expect(res.body.error).toEqual('Not found');
+      });
+  });
+
+  it('should return 404 for GET invalid user id', () => {
+    return request(server)
+      .get(`${BASE_URL}/invalid-user-id`)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.error).toEqual('Bad request');
+      });
+  });
+
+  it('should return 400 for POST user with invalid data', () => {
+    const invalidUserData: Partial<IUser> = {
+      username: 'Chuck Norris',
+      age: 1,
+    };
+
+    return request(server)
+      .post(BASE_URL)
+      .send(invalidUserData)
+      .expect(400)
+      .then((res) => {
+        expect(res.body.error).toEqual('Bad request');
+      });
+  });
+
+  it('should return 404 for PUT user with non-existent id', () => {
+    return request(server)
+      .put(`${BASE_URL}/${v4()}`)
+      .send({ username: 'newUsername' })
+      .expect(404)
+      .then((res) => {
+        expect(res.body.error).toEqual('Not found');
+      });
+  });
+
+  it('should return 404 for DELETE user with non-existent id', () => {
+    return request(server).delete(`${BASE_URL}/${v4()}`).expect(404);
   });
 });
